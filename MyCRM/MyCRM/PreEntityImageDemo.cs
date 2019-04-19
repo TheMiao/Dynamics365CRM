1c581df1b8e9e58da5c8201f908847a58b1c17d2
Miao Wang <Miao.Wang@outlook.com.au>
2019-04-19 10:14:54 +0800

    Adjusing the code

--- MyCRM/MyCRM/PreEntityImageDemo.cs
+++ MyCRM/MyCRM/PreEntityImageDemo.cs
@@ -15,24 +15,20 @@ namespace MyPlugins
             // Extract the tracing service for use in debugging sandboxed plug-ins.  
             // If you are not registering the plug-in in the sandbox, then you do  
             // not have to add any tracing service related code.  
-            ITracingService tracingService =
-                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
             // Obtain the execution context from the service provider.  
-            IPluginExecutionContext context = (IPluginExecutionContext)
-                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
             // Obtain the organization service reference which you will need for  
             // web service calls.  
-            IOrganizationServiceFactory serviceFactory =
-                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
 
 
             // The InputParameters collection contains all the data passed in the message request.  
-            if (context.InputParameters.Contains("Target") &&
-                context.InputParameters["Target"] is Entity)
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
             {
                 // Obtain the target entity from the input parameters.  
                 Entity entity = (Entity)context.InputParameters["Target"];
@@ -49,7 +45,7 @@ namespace MyPlugins
                     // This is modified business phone number
                     string modifiedBusinessPhone = entity.Attributes["telephone1"].ToString();
 
-                    
+
                     Entity preImage = (Entity)context.PreEntityImages["PreImage"];
                     Entity postImage = (Entity)context.PostEntityImages["PostImage"];
 
