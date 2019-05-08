0a13e01b0121c4a2b0f394c25c4fecc7d1b1ca9f
Miao Wang <Miao.Wang@outlook.com.au>
2019-05-08 11:07:49 +0800

    Adding retrieve custom entity code

--- MyCRM/MyCRM/RetrieveCustomEntities.cs
+++ MyCRM/MyCRM/RetrieveCustomEntities.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace MyCRM
 {
-    class RetrieveCustomEntities
+    public class RetrieveCustomEntities
     {
+        public OrganizationServiceProxy serviceProxy { get; set; }
+
+        public void RetrieveustomEntity()
+        {
+            var clientCredentials = new ClientCredentials();
+            clientCredentials.UserName.UserName = "your user name";
+            clientCredentials.UserName.Password = "Your Password";
+
+            using (serviceProxy = new OrganizationServiceProxy(new Uri("url"), null, clientCredentials, null))
+            {
+                serviceProxy.EnableProxyTypes();
+                var query = new QueryExpression("entity local name");
+                query.ColumnSet = new ColumnSet();
+                query.ColumnSet.AddColumn("condition");
+                var retrievedValue = serviceProxy.RetrieveMultiple(query);
+            }
+        }
     }
 }
