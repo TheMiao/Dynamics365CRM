c9e43f2dc8f6f31bd189fcc5b063d40a8e7b7724
Miao Wang <Miao.Wang@outlook.com.au>
2019-05-08 11:09:11 +0800

    Leaving comments

--- MyCRM/MyCRM/RetrieveCustomEntities.cs
+++ MyCRM/MyCRM/RetrieveCustomEntities.cs
@@ -16,13 +16,17 @@ namespace MyCRM
         public void RetrieveustomEntity()
         {
             var clientCredentials = new ClientCredentials();
+            // input the user name
             clientCredentials.UserName.UserName = "your user name";
+            // input the pass word
             clientCredentials.UserName.Password = "Your Password";
 
+            // input your instance svc url
             using (serviceProxy = new OrganizationServiceProxy(new Uri("url"), null, clientCredentials, null))
             {
                 serviceProxy.EnableProxyTypes();
-                var query = new QueryExpression("entity local name");
+                // input the logical name of the entity
+                var query = new QueryExpression("entity logical name");
                 query.ColumnSet = new ColumnSet();
                 query.ColumnSet.AddColumn("condition");
                 var retrievedValue = serviceProxy.RetrieveMultiple(query);
