f31676f4e0730025be49372a84035b445ede0933
Chris Wang <chrismiaow@gmail.com>
2019-06-19 22:11:40 +0800

    Adding functions

--- MyCRM/ContactScripts.js
+++ MyCRM/ContactScripts.js
@@ -1,1 +1,11 @@
-// JavaScript source code
+function ContactLoad() {
+    alert("This is Onload event of From");
+}
+
+function ContactSave() {
+    alert("This is Save event of Form");
+}
+
+function EmailOnChange() {
+    alert("This is OnChange event of email attribute");
+}
