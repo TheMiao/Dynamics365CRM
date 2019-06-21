377bb6869c9259f4c542fb2329132dca5577d0e8
Chris Wang <chrismiaow@gmail.com>
2019-06-21 14:34:35 +0800

    adding JS create an entity in CRM

--- MyCRM/ContactScripts.js
+++ MyCRM/ContactScripts.js
@@ -17,3 +17,28 @@ function DisplayHelloWorld(executionContext) {
 
     alert("Hello World " + firstName);
 }
+
+// Create an entity in CRM
+function CreateEntity(clientURL, entityType, entityData) {
+    var req = new XMLHttpRequest();
+    req.open("POST", encodeURI(clientURL + "/api/data/v9.0/" + entityType), true);
+    req.setRequestHeader("Accept", "application/json");
+    req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    req.setRequestHeader("OData-MaxVersion", "4.0");
+    req.setRequestHeader("OData-Version", "4.0");
+
+    req.onreadystatechange = function () {
+        if (this.readyState === 4 /* complete */) {
+            req.onreadystatechange = null; // avoids memory leaks
+            if (this.status === 204) {
+                var entityUri = this.getResponseHeader("ODta=EntityId");
+                console.log("Created " + entityType + " with URI: " + entityUri);
+            }
+            else {
+                var error = JSON.parse(this.response).error;
+                console.log(error.message);
+            }
+        }
+    };
+    req.send(JSON.stringify(entityData));
+}
