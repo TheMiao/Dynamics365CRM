97872dd94423bd61f3dae27333b774bf6e08277d
Chris Wang <chrismiaow@gmail.com>
2019-07-09 11:06:40 +0800

    Adding retrieve lookup

--- MyCRM/ContactScripts.js
+++ MyCRM/ContactScripts.js
@@ -24,9 +24,23 @@ var Sdk = window.Sdk || {};
     function () {
         this.formOnLoad = function (executionContext) {
             var formContext = executionContext.getFormContext();
-            var firstName = formContext.getAttribute("firstname").getValue();
-            alert("Hello World " + firstName);
+            //var firstName = formContext.getAttribute("firstname").getValue();
+            //alert("Hello World " + firstName);
+
+            var lookupAccountArray = formContext.getAttribute("parentcustomerid").getValue();
+
+            if (lookupAccountArray !== null && lookupAccountArray[0] != null) {
+                // you will get entity type, name and id
+                var accountGuid = lookupAccountArray[0].id;
+                var accountName = lookupAccountArray[0].name;
+                var accountType = lookupAccountArray[0].entityType;
+
+                formContext.ui.setFormNotification("Guid of the Account: " + accountGuid, "Info", "notification1");
+                formContext.ui.setFormNotification("Name of the Account: " + accountName, "Info", "notification2");
+                formContext.ui.setFormNotification("Type of the Account: " + accountType, "Info", "notification3");
+            }
         };
+
 
         this.shippingMethodOnChange = function (executionContext) {
             var formContext = executionContext.getFormContext();
@@ -35,7 +49,7 @@ var Sdk = window.Sdk || {};
             } else {
                 formContext.getControl("address1_freighttermscode").setDisabled(false);
             }
-        }
+        };
     }
 ).call(Sdk);
 
